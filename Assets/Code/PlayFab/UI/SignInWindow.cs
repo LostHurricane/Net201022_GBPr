6dc01379c432c39bf82df0f1fbccc7efc1ba2b25
Nikolay Vetrov <77938349+LostHurricane@users.noreply.github.com>
2023-12-07 03:48:29 +0300

    HW

--- Assets/Code/PlayFab/UI/SignInWindow.cs
+++ Assets/Code/PlayFab/UI/SignInWindow.cs
@@ -1,26 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
-using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class SignInWindow : AccountDataWindowBase
 {
     [SerializeField]
     private Button _signInButton;
 
+    [SerializeField]
+    private Canvas _waitCanvas;
+    private bool _error;
+
     protected override void SubscriptionElementsUI()
     {
         base.SubscriptionElementsUI();
 
-        _signInButton.onClick.AddListener(SignIn);
+        _signInButton.onClick.AddListener(Test);
+    }
+
+    private void Test()
+    {
+        AsyncSignIn();
     }
 
     private void SignIn()
     {
+        _error = false;
         var request = new LoginWithPlayFabRequest
         {
             Username = _username,
@@ -30,14 +37,35 @@ public class SignInWindow : AccountDataWindowBase
         PlayFabClientAPI.LoginWithPlayFab(request, ProcessResult, ProcessError);
     }
 
+    async Task AsyncSignIn()
+    {
+        SignIn();
+        DisplayWaitSign(true);
+        
+        while (!PlayFabClientAPI.IsClientLoggedIn() && !_error)
+        {
+            await Task.Yield();
+
+        }
+        DisplayWaitSign(false);
+
+        Debug.Log($"wait is finished");
+    }
+
+    private void DisplayWaitSign(bool v)
+    {
+        _waitCanvas.enabled = v;
+    }
+
     private void ProcessResult(LoginResult result)
     {
         Debug.Log($"Sign in succces: {_username}");
         EnterInGameScene();
     }
 
-    private void ProcessError(PlayFab.PlayFabError error)
+    private void ProcessError(PlayFabError error)
     {
+        _error = true;
         var result = error.GenerateErrorReport();
         Debug.Log($"Error!\n{result}");
 
