1ad480dacff957ed369fb875279ce1d266610d1f
Nikolay Vetrov <77938349+LostHurricane@users.noreply.github.com>
2024-09-26 00:20:52 +0300

    lesson part

--- Assets/Code/PlayFab/PlayFabLogin.cs
+++ Assets/Code/PlayFab/PlayFabLogin.cs
@@ -3,6 +3,7 @@ using PlayFab.ClientModels;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -61,6 +62,88 @@ public class PlayFabLogin : MonoBehaviour
         Debug.Log(message);
         OnLoginSuccesEvent?.Invoke(message);
 
+        SetUserData(result.PlayFabId);
+        //MakePurchase();
+        GetInventory();
+    }
+
+    private void GetInventory()
+    {
+        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(),
+            result => ShowInventory(result.Inventory), OnLoginError);
+
+    }
+
+    private void ShowInventory(List<ItemInstance> inventory)
+    {
+        var first = inventory.First();
+        Debug.Log($"inventory, item {first.DisplayName}, id {first.ItemInstanceId}");
+        UseItem(first.ItemInstanceId);
+    }
+
+    private void UseItem(string itemInstanceId)
+    {
+        PlayFabClientAPI.ConsumeItem(new ConsumeItemRequest
+        {
+            ItemInstanceId = itemInstanceId,
+            ConsumeCount = 1
+        }, 
+        result => 
+        {
+            Debug.Log("Item COnsumed");
+        }, OnLoginError);
+    }
+
+    private void MakePurchase()
+    {
+        PlayFabClientAPI.PurchaseItem(new PurchaseItemRequest
+        {
+            CatalogVersion = "geometricduelist_catalog",
+            ItemId = "optionalskin_id",
+            Price = 100,
+            VirtualCurrency = "BC"
+        },
+        result =>
+        {
+            Debug.Log("MakePurchase succes");
+        },
+        OnLoginError
+        );
+    }
+
+    private void SetUserData(string playFabId)
+    {
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
+        {
+            Data = new Dictionary<string, string>
+            {
+                {"time_receve_daily_reward", DateTime.UtcNow.ToString() }
+            }
+        },
+        result =>
+        {
+            Debug.Log("Set user Data");
+            GetUserData(playFabId, "time_receve_daily_reward");
+        }, OnLoginError
+        );
+    }
+
+    private void GetUserData(string playFabId, string v)
+    {
+        PlayFabClientAPI.GetUserData(new()
+        {
+            PlayFabId = playFabId
+        },
+        result =>
+        {
+            Debug.Log("Result");
+            if (result.Data.ContainsKey(v)) 
+            {
+                Debug.Log ($"key {v}, result {result.Data[v].Value}");
+            }
+        },
+        OnLoginError
+        );
     }
 
     private void OnLoginError(PlayFabError error)
